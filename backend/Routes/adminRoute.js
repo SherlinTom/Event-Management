65d0ebaa83411b9074fb0d147826bd3e88817013
SHRLN <sherlinaifer@gmail.com>
2025-04-05 22:02:02 +0530

    some chanes occured

--- backend/Routes/adminRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { UserAuthenticate } = require('../Middlewares/authenticate');
-const { getAllUsers, removeUsers, projectStatistics } = require('../Controllers/adminController');
-const router = express.Router();
-
-router.route('/all-users').get(UserAuthenticate,getAllUsers);
-router.route('/delete-user/:id').delete(UserAuthenticate,removeUsers);
-router.route('/statistics').get(UserAuthenticate,projectStatistics);
-module.exports = router;
